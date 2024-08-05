c79457433c5c6d472c43f67c34d7f7f57ed7e291
irfan2602 <meirfan26@gmail.com>
2024-08-06 01:59:28 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GetData from './components/GetData';
+import AddTodoList from './components/AddTodoList';
 
 function App() {
   return (
     <div className="App">
       <GetData />
+      <AddTodoList />
     </div>
   );
 }
---
+++ src/components/AddTodoList.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const AddTodoList = () => {
+    const [todoList, setTodoList] = useState<string[]>([])
+    const [inputVal, setInputVal] = useState('')
+    const handleAddTodoList = (e: any) => {
+        e.preventDefault()
+        setTodoList((pre) => [...pre, inputVal])
+        setInputVal('')
+    }
+    const handleKeyEvent = (e: any) => {
+        if(e.keyCode === 13){
+            handleAddTodoList(e)
+        }
+    }
+
+    return (
+        <div>
+            <input type='text' name="todoList" value={inputVal} onKeyDown={handleKeyEvent} onChange={(e) => setInputVal(e.target.value)} />
+            <button type='submit' onClick={handleAddTodoList} >Add Item</button>
+            <hr />
+            {
+                todoList && todoList.map((data, i) => {
+                    return (
+                        <>
+                            <strong key={i} > {data} </strong><br/>
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default AddTodoList
